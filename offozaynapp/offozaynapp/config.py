229d6d0c6669abf037083ba1f04bd6f9e0eb9265
ozaaayn <azin.faghihi@gmail.com>
2021-10-23 22:57:06 -0400

    modify file path

--- offozaynapp/offozaynapp/config.py
+++ offozaynapp/offozaynapp/config.py
@@ -4,4 +4,5 @@ sheet_url = 'https://docs.google.com/spreadsheets/d/1-0B0CN3y-K0CYoPCHpsh2TKpDP3
 nodes = "https://drive.google.com/file/d/1-0cpXZHUDGToKKlP_niCbjwBfQK3urSz/view?usp=sharing"
 edges = "https://drive.google.com/file/d/14BW0OXkbN7BgwCTZ2YpTATAL5PBRRGRs/view?usp=sharing"
 lat_lon_url = 'https://drive.google.com/file/d/10l-06L2waxvG6pKbCwuX1wtPtrPjgPO4/view?usp=sharing'
-lat_lon_url = 'https://drive.google.com/file/d/1j-q6mn1V5ZI1iuDcqGS8V5Qmv2z1l5c0/view?usp=sharing'
+lat_lon_url = 'https://drive.google.com/file/d/13B1B_6tt_-mgt2kD719ZF_ElvPn34OKp/view?usp=sharing'
+
